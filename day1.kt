52bf380b342b74fac40391e6465f1153aa3fec81
Pranshu12218865 <118091957+Pranshu12218865@users.noreply.github.com>
2024-08-25 00:39:43 +0530

    Add files via upload

---
+++ day1.kt
@@ -0,0 +1,14 @@
+fun main(args:Array<String>){
+    // val a="Hello World!!!!!"
+        // print(a)
+    //   var b=23
+    //   var c=33
+    //     b++
+    //     print(b)
+    // Checking call from interview
+    val marksAbove65=false
+    val knowsProgramming=false
+    val callForInterview=marksAbove65 || knowsProgramming
+    print(callForInterview)
+
+}
---
+++ ifelse.kt
@@ -0,0 +1,9 @@
+fun main(args:Array<String>){
+    val k=false
+    if(k==true){
+        print("Berry Good")
+    }
+    else{
+        print("Not Good")
+    }
+}
